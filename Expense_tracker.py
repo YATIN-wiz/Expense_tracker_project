1de5427a4d45d0f4713f1f809cd9a4049001b08b
Your Name <you@example.com>
2024-07-31 18:55:56 +0530

    Creating expense list

--- Expense_tracker.py
+++ Expense_tracker.py
@@ -1,111 +1,12 @@
 import json
 
-travel,grocery,entertainment,medicines,rent,books = 0,0,0,0,0,0
-
-amt = 0
-
-"""while(True):
-    print("1. Adding a new expense")
-    print("2. Save to a file")
-    print("3. Load from a file")
-    print("4. Display expense")
-    print("5. Exit")
-    dic = {"Travel:", travel,"Groceries:", grocery,"Entertainment:", entertainment,"Medicine", medicines,"Rent:", rent,"Book:", books}
-
-    choice = int(input("Enter your choice[1-5]: "))
-    #travel,grocery,entertainment,medicines,rent,books = 0
-
-    if choice == 1:
-        while(1):
-            print("1. Travel")
-            print("2. Groceries")
-            print("3. Entertainment")
-            print("4. Medicines")
-            print("5. Rent")
-            print("6. Books")
-            print("7. Exit")
-           
-           #travel,grocery,entertainment,medicines,rent,books = 0
-            ch = int(input("Enter choice:"))
-            count = 0
-
-            if choice == 1:
-                amt = int(input("Enter your amount:"))
-                travel += amt
-                
-            elif choice == 2:
-                amt = int(input("Enter your amount:"))
-                grocery += amt
-               
-            elif choice == 3:
-                amt = int(input("Enter your amount:"))
-                entertainment += amt
-               
-            elif choice == 4:
-                amt = int(input("Enter your amount:"))
-                medicines += amt
-                
-            elif choice == 5:
-                amt = int(input("Enter your amount:"))
-                rent += amt 
-                       
-            elif choice == 6:
-                amt = int(input("Enter your amoun:"))
-                books += amt
-           
-                
-            elif choice == 7:
-                print("Exit!")
-                break
-        
-   
-
-
-    elif choice == 2:
-        filename = input("Enter filename")
-        fileobj = open("filename.json", "w")
-        json.dump(dic , fileobj)
-
-    elif choice == 3:
-        filename = input("Enter filename")
-        fileobj = open("filename.json", "r")
-        data = json.load(fileobj)
-        print(data)
-
-    elif choice == 4:
-      
-        for k, v in dic:
-            print(k, ":", v)
-        break
-    elif choice == 5:
-        break
-
-    else:
-        print("Invalid choice!")"""
-        
-
-''' print("Total expenses:")
-        #if(travel != 0):
-        print(travel, " on travel. ")
-        #elif (grocery != 0):
-        print( grocery, " on groceries.")
-        #elif (entertainment != 0):
-        print(entertainment, " on entertainment.")
-        #elif (medicines != 0):
-        print(medicines, " on medicines.")
-        #elif (rent != 0):
-        print(rent, " on rent.")
-        #elif (books != 0):
-        print(books, " on books.")'''
-
 class Expense:
     
-    def __init__(self, amount, cat, file, dic):
+    def __init__(self, amount, cat):
         self.amount = int(amount)
         
         print(category)
         self.cat = int(cat)
-        self.file = file
         self.dic = {category[self.cat]: self.amount}
      
 
@@ -121,9 +22,11 @@ class Expense:
         #self.dic = data
 
     def display(self):
+       # for i in expense:
+           # print(i)
         for k, v in self.dic.items():
             print(k, ":", v )
-
+expense = []
 while(True):
     print("1. Adding a new expense")
     print("2. Save to a file")
@@ -137,17 +40,18 @@ while(True):
         category = ["0. Travel", "1. Groceries", "2. Medicines", "3. Rent"]
         print(category)
         cat = int(input("Enter category: "))
-        ex = Expense(amount, cat , "expense", {})
+        expense.append(Expense(amount, cat ))
         #ex = Expense(1000, "travel" , "expense", {})
     
     elif choice == 2:
-        ex.save()
+        expense.save()
 
     elif choice == 3:
-        ex.load()
+        expense.load()
 
     elif choice == 4:
-        ex.display()
+       
+        expense.display()
 
     elif choice == 5:
         break
