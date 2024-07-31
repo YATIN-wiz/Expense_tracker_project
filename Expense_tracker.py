169261c1349d63ce98fe156893565970cb3340e7
noble <nblphilip7@gmail.com>
2024-07-31 19:17:39 +0530

    adding and printing expense

--- Expense_tracker.py
+++ Expense_tracker.py
@@ -1,14 +1,12 @@
 import json
 
 class Expense:
-    
+    category = ["Travel", "Groceries", "Medicines", "Rent"]
     def __init__(self, amount, cat):
-        self.amount = int(amount)
-        
-        print(category)
-        self.cat = int(cat)
-        self.dic = {category[self.cat]: self.amount}
-     
+        self.data = {
+            "amount": amount,
+            "cat": cat
+        }
 
     def save(self):
         name = self.file
@@ -22,13 +20,12 @@ class Expense:
         #self.dic = data
 
     def display(self):
-       # for i in expense:
-           # print(i)
-        for k, v in self.dic.items():
-            print(k, ":", v )
-expense = []
+       print(f'{self.data["amount"]} on {self.category[self.data["cat"]]}')
+
+#main starts here
+expenses = []
 while(True):
-    print("1. Adding a new expense")
+    print("1. Add a new expense")
     print("2. Save to a file")
     print("3. Load from a file")
     print("4. Display expense")
@@ -37,21 +34,21 @@ while(True):
     
     if choice == 1:
         amount = input("Enter amount: ")
-        category = ["0. Travel", "1. Groceries", "2. Medicines", "3. Rent"]
-        print(category)
+        for i, cat in enumerate(Expense.category):
+            print(i, cat)
         cat = int(input("Enter category: "))
-        expense.append(Expense(amount, cat ))
+        expenses.append(Expense(amount, cat))
         #ex = Expense(1000, "travel" , "expense", {})
     
     elif choice == 2:
-        expense.save()
+        expenses.save()
 
     elif choice == 3:
-        expense.load()
+        expenses.load()
 
     elif choice == 4:
-       
-        expense.display()
+       for expense in expenses:
+            expense.display()
 
     elif choice == 5:
         break
